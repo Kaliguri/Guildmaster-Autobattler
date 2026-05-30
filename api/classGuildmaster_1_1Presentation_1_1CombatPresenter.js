2ac90acc7a696f22f2a6d50af153f1a68c81d2af
Kaliguri <Kaliguri@users.noreply.github.com>
2026-05-30 13:30:53 +0000

    deploy: 0985710176c8237384207d266075a5fce1d6b336

--- api/classGuildmaster_1_1Presentation_1_1CombatPresenter.js
+++ api/classGuildmaster_1_1Presentation_1_1CombatPresenter.js
@@ -1,4 +1,4 @@
 var classGuildmaster_1_1Presentation_1_1CombatPresenter =
 [
-    [ "Construct", "classGuildmaster_1_1Presentation_1_1CombatPresenter.html#a6405ee3feb7ff844fc37029cf446ec41", null ]
+    [ "Construct", "classGuildmaster_1_1Presentation_1_1CombatPresenter.html#a63418389790f6cb76161f1795a756570", null ]
 ];
